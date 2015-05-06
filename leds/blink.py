860643c488e3490078f0edb4eee7c96a72e958fc
Nicolas Malbran <nmalbran@gmail.com>
2015-05-06 00:40:40 -0300

    Added 2 led blink.

--- leds/blink.py
+++ leds/blink.py
@@ -17,8 +17,24 @@ def blink(delta=1, channel=2):
 			break
 
 
+def blink2(delta=1, c1=2, c2=3):
+	led1 = Led(c1)
+	led2 = Led(c2)
+	while True:
+		try:
+			led1.on()
+			led2.off()
+			time.sleep(delta)
+			led1.off()
+			led2.on()
+			time.sleep(delta)
+		except KeyboardInterrupt as e:
+			print('Finished')
+			break
+
+
 if __name__ == '__main__':
 	delta = get_cli_float(1, 1.0)
-	blink(delta)
+	blink2(delta)
 
 
