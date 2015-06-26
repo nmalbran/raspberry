2f212d87f69c1ca2349cf35f3889dcb9fec7f9be
Nicolas Malbran <nmalbran@gmail.com>
2015-06-26 01:36:55 -0300

    Added marshalling

--- temp/app.py
+++ temp/app.py
@@ -1,14 +1,25 @@
 from flask import Flask
+from db import Weather
 import Adafruit_DHT
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     msg = 'Try again :('
     humidity, temperature = _get_data()
     if humidity is not None and temperature is not None:
         msg = 'Temp={0:0.1f}*C  Humidity={1:0.1f}%\n'.format(temperature, humidity)
     return msg
+
+@app.route("/last")
+def last():
+    temps = Weather.select().order_by(Weather.timestamp.desc()).limit(100)
+    msg = ''
+    for w in temps:
+        msg += '{0} <br>\n'.format(w)
+    return msg
+
 
 def _get_data(pin=4, sensor=Adafruit_DHT.DHT11):
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
--- temp/db.py
+++ temp/db.py
@@ -1,18 +1,38 @@
 from datetime import datetime
-from peewee import *
+import peewee as pw
+from marshmallow import Schema, fields
 
-db = SqliteDatabase('temp.db')
+db = pw.SqliteDatabase('temp.db')
 
-class Weather(Model):
-    timestamp = DateTimeField(default=datetime.now)
-    temperature = FloatField()
-    humidity = FloatField()
+class Weather(pw.Model):
+    timestamp = pw.DateTimeField(default=datetime.now)
+    temperature = pw.FloatField()
+    humidity = pw.FloatField()
 
-#    def __str__(self):
-#        return "" % (self.temperature, self.humidity)
+    def get_formatted_ts(self):
+        return "%d/%d/%d %d:%d:%d" % (self.timestamp.year, self.timestamp.month, self.timestamp.day, self.timestamp.hour, self.timestamp.minute, self.timestamp.second)
+
+    def __str__(self):
+        #ts = datetime.strftime("%H:%M:%S", self.timestamp)
+        return "%s | T %d*C | H %d%%" % (self.get_formatted_ts(), self.temperature, self.humidity)
 
     class Meta:
         database = db
+    
+    def to_dict(self):
+        schema = WeatherSchema()
+        return schema.dumps(self).data
+
+    @classmethod
+    def to_dict_list(cls, weather_list):
+        schema = WeatherSchema(many=True)
+        return schema.dump(weather_list).data
+
+
+class WeatherSchema(Schema):
+    timestamp = fields.DateTime()
+    temperature = fields.Number()
+    humidity = fields.Number()
 
 
 def create_db():
